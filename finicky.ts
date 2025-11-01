a209a24fb7cb9ecd40385a1f988120c53bc461c6
Alan Berdinelli <alan.berdinelli@naranjax.com>
2025-11-01 20:07:42 -0300

    force open selector in default browser

--- finicky.ts
+++ finicky.ts
@@ -1,5 +1,8 @@
 import type { FinickyConfig } from "/Applications/Finicky.app/Contents/Resources/finicky.d.ts";
 
+// Only change if you know what you're doing!
+const SELECTOR_PORT = 5400;
+
 // Set your default browser here
 // This will also open the browser selector
 const DEFAULT_BROWSER = 'Safari';
@@ -12,6 +15,9 @@ const filters = {
   // This will open in your default browser
   personal: [],
 
+  // Add pages here to open the browser selector
+  ask: [],
+
   // For opening in Google Chrome
   chrome: [],
 
@@ -19,12 +25,21 @@ const filters = {
   firefox: []
 };
 
+const redirect = (url: URL) => {
+  return `http://localhost:${SELECTOR_PORT}/?url=${encodeURIComponent(url.toString())}`;
+}
+
 export default {
   defaultBrowser: DEFAULT_BROWSER,
   rewrite: [
     {
       match: '*',
       url: (url) => {
+        // Open browser selector if matches with ask array
+        if (filters.ask.some(thing => url.toString().includes(thing))) {
+          return redirect(url);
+        }
+
         // If contains a recognized url, let other config decide
         if (Object.values(filters).flat().some(thing => url.toString().includes(thing))) {
           return url;
@@ -35,12 +50,22 @@ export default {
           return url;
         }
 
-        // Redirect to browser chooser
-        return `http://localhost:4400/?url=${encodeURIComponent(url.toString())}`;
+        // Fallback to browser chooser
+        return redirect(url);
       }
     }
   ],
   handlers: [
+    {
+      match: (url) => {
+        if (url.toString().startsWith(`http://localhost:${SELECTOR_PORT}`)) {
+          return true;
+        }
+
+        return false;
+      },
+      browser: DEFAULT_BROWSER
+    },
     {
       match: (url) => {
         if (!url || !url.href || !url.host || !url.pathname) {
